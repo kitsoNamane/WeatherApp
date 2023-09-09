1d4f1f77be7a5793cecaaaf5933404904df2659d
Kitso Gidion Namane <kitsogideonnamane@gmail.com>
2023-09-09 12:23:38 +0200

    refactor: reduce clutter in ContentView

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -12,49 +12,73 @@ struct ContentView: View {
         ZStack {
             BackgroudView(topColor: .blue, bottomColor: Color("lightBlue"))
             VStack(alignment: .center) {
-                Text("Cupertino, CA")
-                .font(.system(size: 32, weight: .medium, design: .default))
-                .foregroundStyle(.white)
-                .padding()
-                
-                VStack(spacing: 8) {
-                    Image(systemName: "cloud.sun.fill")
-                    .renderingMode(.original)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 180, height: 180)
-                    
-                    Text("76°")
-                    .font(.system(size: 70, weight: .medium))
-                    .foregroundStyle(.white)
-                }
-                .padding(.bottom, 40)
+                CityTextView(cityName: "Cupertino, CA")
+                CityWeatherStatus(weatherSymbol: "cloud.sun.fill", temperature: 76)
                 
                 HStack(spacing: 20) {
-                    WeatherDayView(dayOfWeek: "TUE", imageName: "sun.max.fill", temperature: 74)
+                    WeatherDayView(dayOfWeek: "TUE", weatherSymbol: "sun.max.fill", temperature: 74)
                     
-                    WeatherDayView(dayOfWeek: "WED", imageName: "cloud.sun.fill", temperature: 78)
+                    WeatherDayView(dayOfWeek: "WED", weatherSymbol: "cloud.sun.fill", temperature: 78)
                     
-                    WeatherDayView(dayOfWeek: "THU", imageName: "cloud.rain.fill", temperature: 64)
+                    WeatherDayView(dayOfWeek: "THU", weatherSymbol: "cloud.rain.fill", temperature: 64)
                     
-                    WeatherDayView(dayOfWeek: "FRI", imageName: "cloud.hail.fill", temperature: 50)
+                    WeatherDayView(dayOfWeek: "FRI", weatherSymbol: "cloud.hail.fill", temperature: 50)
                     
-                    WeatherDayView(dayOfWeek: "SAT", imageName: "cloud.snow.fill", temperature: 44)
+                    WeatherDayView(dayOfWeek: "SAT", weatherSymbol: "cloud.snow.fill", temperature: 44)
                 }
                 Spacer()
                 
                 Button {
                     print("tapped")
                 } label: {
-                    Text("Change Day Time")
-                    .frame(width: 280, height: 50)
-                    .background(Color.white)
-                    .font(.system(size: 20, weight: .bold, design: .default))
-                    .cornerRadius(10)
+                   WeatherButton(title: "Change Day Time", textColor: .blue, backgroudColor: .white)
                 }
                 Spacer()
             }
         }
+    }
+}
+
+struct CityTextView: View {
+    var cityName: String
+    var body: some View {
+         Text(cityName)
+            .font(.system(size: 32, weight: .medium, design: .default))
+            .foregroundStyle(.white)
+            .padding()
+    }
+}
+
+struct CityWeatherStatus: View {
+    var weatherSymbol: String
+    var temperature: Int
+    var body: some View {
+        VStack(spacing: 8) {
+                    Image(systemName: weatherSymbol)
+                    .renderingMode(.original)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 180, height: 180)
+                    
+                    Text("\(temperature)°")
+                    .font(.system(size: 70, weight: .medium))
+                    .foregroundStyle(.white)
+                }
+                .padding(.bottom, 40)
+    }
+}
+
+struct WeatherButton: View {
+    var title: String
+    var textColor: Color
+    var backgroudColor: Color
+    var body: some View {
+         Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroudColor)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(10)
+            .foregroundColor(textColor)
     }
 }
 
@@ -70,13 +94,13 @@ struct BackgroudView: View {
 
 struct WeatherDayView: View {
     var dayOfWeek: String
-    var imageName: String
+    var weatherSymbol: String
     var temperature: Int
     
     var body: some View {
         VStack(spacing: 4) {
             Text(dayOfWeek)
-            Image(systemName: imageName)
+            Image(systemName: weatherSymbol)
             .renderingMode(.original)
             .resizable()
             .aspectRatio(contentMode: .fit)
